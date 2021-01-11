8bf1dee32d79c2f3f94953275a2f500d7ae38553
AYAN BANERJEE <48727074+ayanbabusona@users.noreply.github.com>
2021-01-11 19:43:10 +0530

    third commit

---
+++ regex pattern/valid_username_regular_expression.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "(^[a-zA-Z][0-9_|a-zA-Z]{7,29})";
+    //public static final String regularExpression = null;
+}
+
+
+public class Solution {
+    private static final Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }           
+        }
+    }
+}
